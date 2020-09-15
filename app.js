299b3cb7b76dee27b7aebe058e4be41259dbdde9
LBrasolin <brasolinlisandro0@gmail.com>
2020-09-15 19:14:07 -0300

    Agregué app.js, index.html, index.css

---
+++ app.js
@@ -0,0 +1,20 @@
+let express = require('express');
+let app = express();
+
+app.listen(3000, (req, res) => {
+    console.log("funcando");
+});
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/register.html', (req, res) => {
+    res.sendFile(__dirname + '/register.html')
+})
+
+app.get('/login.html', (req, res) => {
+    res.sendFile(__dirname + '/login.html')
+})
