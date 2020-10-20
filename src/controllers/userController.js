7c17ed0d12f29add3e8c44cac4ab0e7025667ad7
LBrasolin <brasolinlisandro0@gmail.com>
2020-10-20 17:19:16 -0300

    Agregar usuarios, validator a medio funcionar

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,12 @@
 const fs = require('fs');
+const bcrypt = require('bcryptjs');
+const path = require('path');
+const { validationResult } = require('express-validator');
+
+let readJson = () => {
+    const leer = fs.readFileSync(path.resolve(__dirname + '/db-villera.json'));
+    return JSON.parse(leer);
+}
 
 exports.showRegister = (req, res) => {
     res.render('users/register');
@@ -14,8 +22,33 @@ exports.processLogin = (req, res) => {
 
 exports.processRegister = (req, res) => {
     
-    const { name, surname, email, password, confirm__password } = req.body;
+    // const errors = validationResult(req);
+    //if(errors.isEmpty()) {
+        let passwordHash = bcrypt.hashSync(req.body.password, Math.floor(Math.random() * 10));
 
-    
+        let errors = validationResult(req);
+        let usuario = {
+            name: req.body.name,
+            surname: req.body.surname,
+            email: req.body.email,
+            password: passwordHash,
+        }
+        
+        let usuarios = readJson();
+        
+        console.log(usuario);
+        console.log(errors);
 
+        usuarios.push(usuario);
+
+        usuarios = JSON.stringify(usuarios, null, " ");
+
+        fs.writeFileSync(path.resolve(__dirname + '/db-villera.json'), usuarios)
+
+        res.send(usuarios);
+        
+        res.redirect('/login');
+    // } else { 
+    // res.render('register', {errors: errors.errors})
+    // }
 };
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/userController');
+const { body, validationResult } = require('express-validator');
 
 router.get('/login', controller.showLogin);
 router.post('/login', controller.processLogin);
 
 router.get('/register', controller.showRegister);
-router.post('/register', controller.processRegister);
+router.post('/register', [
+    body("name")
+    .isEmpty()
+    .isLength({min: 3})
+    .withMessage('Ingresar un nombre válido'),
+    body("surname")
+    .isEmpty()
+    .withMessage('Ingresar un apellido válido')
+    .isString(),
+    body("email")
+    .isEmpty()
+    .withMessage('No deje el campo vacío')
+    .isEmail()
+    .withMessage('Ingresar un mail válido'),
+    body("password")
+    .isEmpty()
+    .withMessage('Su contraseña no debe estar vacía')
+    .isLength({min: 8})
+    .withMessage('Su contraseña es muy corta'),
+], controller.processRegister);
 
 module.exports = router;
