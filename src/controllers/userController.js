109a908938eebdfcb414ef9a26e98068779580d7
LBrasolin <brasolinlisandro0@gmail.com>
2020-11-24 19:16:21 -0300

    Terminado agregar usuarios.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,10 +2,7 @@ const fs = require('fs');
 const bcrypt = require('bcryptjs');
 const path = require('path');
 const { validationResult } = require('express-validator');
-
-let readJson = () => {
-    return JSON.parse(fs.readFileSync(path.resolve(__dirname + '/data/usersDB.json')));
-}
+let db = require("../../database/models")
 
 exports.showRegister = (req, res) => {
     res.render('users/register');
@@ -19,33 +16,18 @@ exports.processLogin = (req, res) => {
 
 };
 
-exports.processRegister = (req, res) => {
-    
+exports.processRegister = async (req, res) => {
     const errors = validationResult(req);
     if(errors.isEmpty()) {
         let passwordHash = bcrypt.hashSync(req.body.password, Math.floor(Math.random() * 10));
-
-        let errors = validationResult(req);
-        let usuario = {
+        let usuario = await db.Users.create({
             name: req.body.name,
             surname: req.body.surname,
             email: req.body.email,
-            password: passwordHash,
-        }
-        
-        let usuarios = readJson();
-        
-    
-        usuarios.push(usuario);
-
-        usuarios = JSON.stringify(usuarios, null, " ");
-
-        fs.writeFileSync(path.resolve(__dirname + '/data/usersDB.json'), usuarios)
-
-        res.send(usuarios);
-        
-        res.redirect('/login');
+            pw_hash: passwordHash
+        })
+    res.json(usuario);
      } else { 
-         res.render('register', {errors: errors.errors})
+        res.render('register', {errors: errors.errors})
      }
 };
