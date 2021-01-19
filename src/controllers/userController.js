56fa217d03ab33262d6f78cacde871bf5e31f3fd
Joaquín <jopiodz00@gmail.com>
2021-01-19 18:29:26 -0300

    Aprolijo userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,8 @@
-const fs = require('fs');
 const bcrypt = require('bcryptjs');
-const path = require('path');
 const { validationResult } = require('express-validator');
-let db = require("../database/models")
-let {Op} = require('sequelize')
+const db = require("../database/models")
+const { Op } = require('sequelize')
+
 exports.showRegister = (req, res) => {
     if(!req.session.usuario) {
         res.render('users/register');
@@ -20,40 +19,42 @@ exports.showLogin = (req, res) => {
 };
 exports.processLogin = async (req, res) => {
     const errors = validationResult(req);
-    if(errors.isEmpty()) {
-        let usuarioLogueado = await db.Users.findAll({
-            where: {
-                email: {[Op.like]: req.body.email}
-            }
-        })
-        usuarioLogueado = usuarioLogueado[0];
-        if(usuarioLogueado != undefined) {
-            bcrypt.compare(req.body.password, usuarioLogueado.pw_hash, (err, result) => {
-                if(err || !result) {
-                    res.render('users/login', {errors: 'Email/Contraseña inválida.'});
-                    return false;
-                } else {
-                    req.session.usuario = usuarioLogueado;
-                    res.redirect('/');
-                    return true;
-                }
-            });
-        } else {
-            res.render('users/login', {errors: 'Email no encontrado.'});
-        }
-    } else {
-    res.render('users/login', errors);
+    if(!errors.isEmpty()) {
+        return res.render('users/login', errors);
     }
-    // código súper, súper en proceso
-};
-exports.processRegister = async (req, res) => {
-    const errors = validationResult(req);
-    let usuarioEncontrar = await db.Users.findAll({
+
+    let usuarioLogueado = await db.Users.findOne({
         where: {
             email: {[Op.like]: req.body.email}
         }
-    }) 
-    if(errors.isEmpty()) {
+    });
+
+    if(usuarioLogueado != undefined) {
+        bcrypt.compare(req.body.password, usuarioLogueado.pw_hash, (err, result) => {
+            if(err || !result) {
+                res.render('users/login', { errors: 'Email/Contraseña inválida.' });
+                return false;
+            } else {
+                req.session.usuario = usuarioLogueado;
+                res.redirect('/');
+                return true;
+            }
+        });
+    } else {
+        res.render('users/login', {errors: 'Email no encontrado.'});
+    }
+};
+exports.processRegister = async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.render('users/register', {errors: errors.errors})
+        }
+        let usuarioEncontrar = await db.Users.findOne({
+            where: {
+                email: {[Op.like]: req.body.email}
+            }
+        }) 
         if(usuarioEncontrar == "") {
             let passwordHash = bcrypt.hashSync(req.body.password, Math.floor(Math.random(1) * 10));
             await db.Users.create({
@@ -66,19 +67,19 @@ exports.processRegister = async (req, res) => {
         } else {
             res.render('error');
         }
-    res.redirect('/user/login')
-     } else { 
-        res.render('users/register', {errors: errors.errors})
-     }
+        return res.redirect('/user/login')
+    } catch (error) {
+        console.log(error);
+    }
 };
 exports.showProfile = (req, res) => {
     if(!req.session.usuario) {
-        res.redirect('/user/login')
+        return res.redirect('/user/login')
     } else {
-        res.render('users/profile');
+        return res.render('users/profile');
     }
 };
 exports.logOut = (req, res) => {
     req.session.destroy();
-    res.redirect('/')
+    return res.redirect('/')
 };
