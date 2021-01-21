b1808142b1ec3c82f11249c0b2d272067a596466
Joaquín <jopiodz00@gmail.com>
2021-01-21 20:38:42 -0300

    prueba 2

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "Ti8LcMpJTu",
+    "password": "84V2b4AkmX",
+    "database": "Ti8LcMpJTu",
+    "host": "remotemysql.com",
     "dialect": "mysql"
   }
 }
