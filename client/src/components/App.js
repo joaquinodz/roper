b2eba36fe015d710da2d146c7e2bcdd3f1891823
Joaquin <jopiodz00@gmail.com>
2021-06-05 14:54:33 -0300

    Fixes styling Header component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,14 @@
+import { Fragment } from "react";
+
 import './App.css';
 
 import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
+    <Fragment>
       <Header />
-    </div>
+    </Fragment>
   );
 }
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,25 +5,32 @@ import './Header.css';
 class Header extends Component {
     render () {
         return (
-            <header className="header">
-                <a href="/">
-                        <img src="../../images/logo-roper.svg" alt="" className="logo" />
-                </a>
-                
-                <div className="line"></div>
-                
+            <header>
+
+                {/* Logo + Linea que lo separa con la barra de búsqueda */}
+                <div className="logo__line">
+                    <a href="/">
+                        <img src="./images/logo-roper.svg" alt="" className="logo" />
+                    </a>
+
+                    <div className="line"></div>
+                </div>
+
+                {/* Barra de Busqueda */}
                 <form action="/search" method="POST" className="buscar">
                     <input type="search" name="search" id="search" placeholder="Buscar..." />
                 </form>
                 
+                {/* Iniciar Sesion - Registrarse - Avatar */}
+                {/* TODO: Convertir en componente */}
                 <div className="usuario">
-                    <img src="../../images/user.png" style={{ "width": "100%" }} alt="" />
+                    <img src="./images/user.png" alt="avatar" />
                     <div className="nologin">
                         <a href="/user/register"><h4>Registrarse</h4></a>
                         <a href="/user/login"><p>Ingresar</p></a>
                     </div>
 
-                    <a href="/carrito"><img src="../../images/cart.png" alt="" className="cart" /></a>
+                    <a href="/carrito"><img src="../../images/cart.png" alt="carrito" className="cart" /></a>
                 </div>
                 
                 <hr></hr>
@@ -36,7 +43,7 @@ class Header extends Component {
                     </ul>
                 </nav>
 
-                <hr className="hr"></hr>
+                <hr></hr>
             </header>
         );
     }
