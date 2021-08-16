c70877a8b1fd066dd607ff9ab622dd7cec8b41c0
Joaquin <jopiodz00@gmail.com>
2021-08-16 20:46:57 -0300

    'BrowserRouter' -> 'HashRouter', for GitHub Pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "pages/Home";
 import Header from "components/Header";
